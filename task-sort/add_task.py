da6750e68d383c1ce174069499ca277453a1db7f
liboqiao1234 <13693338025@163.com>
2020-03-03 20:11:43 +0800

    add a new task

--- task-sort/add_task.py
+++ task-sort/add_task.py
@@ -6,7 +6,7 @@ time=input("请输入截止日期:")
 importance=input("请输入重要程度:")
 old=json.load(open("E:/workspace/mygithubio/liboqiao1234.github.io/task-sort/data.json","r",encoding="UTF-8"))
 data=[name,time,importance]
-old["list"][num-1].append(data)
+old["list"][num-1][2].append(data)
 json_str=json.dumps(old,ensure_ascii=False)
 with open("E:/workspace/mygithubio/liboqiao1234.github.io/task-sort/data.json","w",encoding="utf-8") as file_obj:
     file_obj.write(json_str)
