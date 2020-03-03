ed647ad225062f7840852431b7995f32d030de44
liboqiao1234 <13693338025@163.com>
2020-03-03 16:39:56 +0800

    fix bug

--- task-sort/index.js
+++ task-sort/index.js
@@ -5,9 +5,12 @@ function getList(){
     success:function(res){
       data=res;
       $(".list_number").text(res.user[1]);
+      var a="";
       $(data.list).each(function(index,value){
+        a=a+"<li class='list-group-item'><a href='#' class='list_detail' list_id='"+(index+1)+"'> "+value[0]+"</a></li>";
         alert(index+" "+value);
       });
+      $.("#list_show").val(a);
       alert("欢迎！今天也要努力哦！"+res.user[0]);
     }
   })
