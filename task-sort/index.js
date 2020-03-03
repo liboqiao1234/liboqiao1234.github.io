f096ac98f29b189b6bcbcfb153d975ef05316b5c
liboqiao1234 <13693338025@163.com>
2020-03-03 10:49:13 +0800

    test

--- task-sort/index.js
+++ task-sort/index.js
@@ -2,9 +2,9 @@ function getList(){
   $.ajax({
     url:"data.json",
     success:function(res){
-      res_j=JSON.parse(res);
-      console.log(res_j);
-      alert("欢迎！今天也要努力哦！"+res_j.user[0]);
+      // res_j=JSON.parse(res);
+      // console.log(res_j);
+      alert("欢迎！今天也要努力哦！"+res.user[0]);
     }
   })
 };
