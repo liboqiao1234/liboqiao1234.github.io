55172bb881638465b378aca2a32950806bace4e2
liboqiao1234 <13693338025@163.com>
2020-03-03 16:55:21 +0800

    fix bug

--- task-sort/index.js
+++ task-sort/index.js
@@ -7,7 +7,7 @@ function getList(){
       $(".list_number").text(res.user[1]);
       var a="";
       $(data.list).each(function(index,value){
-        a=a+"<li class='list-group-item'><a href='#' class='list_detail' list_id='"+(index+1)+"'> "+value[0]+"</a></li>";
+        a=a+"<li class='list-group-item'><span class='badge'>"+value[1]+"<a href='#' class='list_detail' list_id="+(index+1)+"> "+value[0]+"</a></li>";
         alert(index+" "+value);
       });
       $("#list_show").html(a);
@@ -21,6 +21,11 @@ $(document).ready(function(){
   getList();
 });
 $(".list_detail").on("click",function(){
+  alert("click!");
   alert($(this).text());
+  var a=0;
+  a=$(this).attr("list_id");
   console.log("click:"+$(this).text());
+  $("#now_list").text(data.list[a-1][0]);
+  $("#now_list_num").text(data.list[a-1][1]);
 });
