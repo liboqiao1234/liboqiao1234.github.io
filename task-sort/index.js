bf79c0d66245f16842c221bdf07e11fcc10ec72d
liboqiao1234 <13693338025@163.com>
2020-03-03 11:02:28 +0800

    fix bug

--- task-sort/index.js
+++ task-sort/index.js
@@ -2,7 +2,7 @@ function getList(){
   $.ajax({
     url:"data.json",
     success:function(res){
-      $(".list_number").val(res.user[1]);
+      $(".list_number").text(res.user[1]);
       alert("欢迎！今天也要努力哦！"+res.user[0]);
     }
   })
