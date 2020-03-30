84bed3c5a57040b3dbaf25d44997e3301d1d2118
liboqiao1234 <13693338025@163.com>
2020-03-30 08:54:38 +0800

    add a new task

--- task-sort/add_task.py
+++ task-sort/add_task.py
@@ -1,15 +1,33 @@
 import json
 import os
-num=int(input("请输入清单号码:"))
-name=input("请输入待办事项:")
-time=input("请输入截止日期:")
-importance=input("请输入重要程度:")
-old=json.load(open("E:/workspace/mygithubio/liboqiao1234.github.io/task-sort/data.json","r",encoding="UTF-8"))
-data=[name,time,importance]
-old["list"][num-1][2].append(data)
-old["list"][num-1][1]=old["list"][num-1][1]+1
-json_str=json.dumps(old,ensure_ascii=False)
-with open("E:/workspace/mygithubio/liboqiao1234.github.io/task-sort/data.json","w",encoding="utf-8") as file_obj:
-    file_obj.write(json_str)
-file_obj.close();
+def add():
+    num=int(input("请输入清单号码:"))
+    name=input("请输入待办事项:")
+    time=input("请输入截止日期:")
+    importance=input("请输入重要程度:")
+    old=json.load(open("E:/workspace/mygithubio/liboqiao1234.github.io/task-sort/data.json","r",encoding="UTF-8"))
+    data=[name,time,importance]
+    old["list"][num-1][2].append(data)
+    old["list"][num-1][1]=old["list"][num-1][1]+1
+    json_str=json.dumps(old,ensure_ascii=False) 
+    with open("E:/workspace/mygithubio/liboqiao1234.github.io/task-sort/data.json","w",encoding="utf-8") as file_obj:
+        file_obj.write(json_str)
+    file_obj.close();
+def finish():
+    old=json.load(open("E:/workspace/mygithubio/liboqiao1234.github.io/task-sort/data.json","r",encoding="UTF-8"))
+    for i in range(0,old["user"][1]):
+        for j in range(0,old["list"][i][1]):
+            print(old["list"][i][2][j][0],old["list"][i][2][j][1])
+        print("清单",i)
+    json_str=json.dumps(old,ensure_ascii=False) 
+    with open("E:/workspace/mygithubio/liboqiao1234.github.io/task-sort/data.json","w",encoding="utf-8") as file_obj:
+        file_obj.write(json_str)
+    file_obj.close();
+
+
+op=int(input("请输入选项:0完成，1增加"))
+if(op==1):
+    add()
+elif(op==0):
+    finish()
 os.system("E:/workspace/mygithubio/liboqiao1234.github.io/task-sort/update.bat")
