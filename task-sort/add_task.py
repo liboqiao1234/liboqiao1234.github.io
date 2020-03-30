8e4ad2dbaae2868c6184e82489bb743029e52c4f
liboqiao1234 <13693338025@163.com>
2020-03-30 09:09:47 +0800

    add a new task

--- task-sort/add_task.py
+++ task-sort/add_task.py
@@ -21,10 +21,18 @@ def finish():
             print(j+1,end=":")
             print(old["list"][i][2][j][0],old["list"][i][2][j][1])
         print()
-    nn=int(input("请输入完成的清单编号\n"))
-    mm=int(input("请输入序号\n"))
-    del old["list"][nn-1][2][mm-1]
-    old["list"][nn-1][1]=old["list"][nn-1][1]-1
+    nn=0
+    while nn!=-1:
+        nn=int(input("请输入完成的清单编号\n"))
+        mm=int(input("请输入序号\n"))
+        del old["list"][nn-1][2][mm-1]
+        old["list"][nn-1][1]=old["list"][nn-1][1]-1
+        for i in range(0,old["user"][1]):
+            print("清单",i+1,old["list"][i][0])
+            for j in range(0,old["list"][i][1]):
+                print(j+1,end=":")
+                print(old["list"][i][2][j][0],old["list"][i][2][j][1])
+            print()
     json_str=json.dumps(old,ensure_ascii=False) 
     with open("E:/workspace/mygithubio/liboqiao1234.github.io/task-sort/data.json","w",encoding="utf-8") as file_obj:
         file_obj.write(json_str)
